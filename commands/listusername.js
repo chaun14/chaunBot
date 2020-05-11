97c1daaf4d74a67583ec9eaca866f731834af142
chaun14 <chaun1403@gmail.com>
2020-05-11 19:21:11 +0200

    bug fix

--- commands/listusername.js
+++ commands/listusername.js
@@ -29,7 +29,7 @@ module.exports.run = async(client, message, args) => {
     FROM
     userNameLogger
     WHERE
-    userId = '${SqlString.escape(args[0])}'
+    userId = ${SqlString.escape(args[0])}
     ORDER BY updateDate ASC
     `
 
