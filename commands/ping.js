d4f8397b99f7e06cc377a2aa7d0e38490ae58fd7
chaun14 <chaun1403@gmail.com>
2020-05-12 15:34:38 +0200

    fix ping cmd

--- commands/ping.js
+++ commands/ping.js
@@ -2,13 +2,13 @@ const db = require("../db.js")
 const Discord = require("discord.js");
 
 
-module.exports.run = async (client, message, args) => {
-  // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+module.exports.run = async(client, message, args) => {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Le ping du bot est de ${m.createdTimestamp - message.createdTimestamp}ms. Ping de l'API ${Math.round(client.ping)}ms`);
-  
-  
+    m.edit(`Pong! Le ping du bot est de ${m.createdTimestamp - message.createdTimestamp}ms. Ping de l'API ${Math.round(client.ws.ping)}ms`);
+
+
 }
 
 
